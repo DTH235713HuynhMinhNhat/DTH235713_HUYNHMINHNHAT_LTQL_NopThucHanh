7513d151a4173b4116e4acb4e19f4cbaf65e7633
admin <admin@LAPTOP-T018HFGU>
2026-02-03 14:28:49 +0700

    ThucHanhBuoi2 thêm frmNhanVien thêm frmKhachHang

---
+++ Buoi2/QuanLyBanHang/form/frmLoaiSanPham.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace QuanLyBanHang.form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtTenLoai = new TextBox();
+            btnThem = new Button();
+            btnXoa = new Button();
+            btnLuu = new Button();
+            btnHuyBo = new Button();
+            btnThoat = new Button();
+            dataGridView = new DataGridView();
+            ID = new DataGridViewTextBoxColumn();
+            TenLoai = new DataGridViewTextBoxColumn();
+            panel2 = new Panel();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            btnSua = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            panel2.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // txtTenLoai
+            // 
+            txtTenLoai.Location = new Point(207, 36);
+            txtTenLoai.Name = "txtTenLoai";
+            txtTenLoai.Size = new Size(558, 23);
+            txtTenLoai.TabIndex = 0;
+            txtTenLoai.TextChanged += textBox1_TextChanged_1;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(195, 67);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(75, 23);
+            btnThem.TabIndex = 3;
+            btnThem.Text = "Thêm ";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.ForeColor = Color.Crimson;
+            btnXoa.Location = new Point(403, 79);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(75, 23);
+            btnXoa.TabIndex = 5;
+            btnXoa.Text = "Xóa ";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnLuu
+            // 
+            btnLuu.ForeColor = SystemColors.Highlight;
+            btnLuu.Location = new Point(500, 79);
+            btnLuu.Name = "btnLuu";
+            btnLuu.Size = new Size(75, 23);
+            btnLuu.TabIndex = 6;
+            btnLuu.Text = "Lưu";
+            btnLuu.UseVisualStyleBackColor = true;
+            btnLuu.Click += btnLuu_Click;
+            // 
+            // btnHuyBo
+            // 
+            btnHuyBo.Location = new Point(593, 79);
+            btnHuyBo.Name = "btnHuyBo";
+            btnHuyBo.Size = new Size(75, 23);
+            btnHuyBo.TabIndex = 7;
+            btnHuyBo.Text = "Hủy bỏ";
+            btnHuyBo.UseVisualStyleBackColor = true;
+            btnHuyBo.Click += btnHuyBo_Click;
+            // 
+            // btnThoat
+            // 
+            btnThoat.Location = new Point(690, 79);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(75, 23);
+            btnThoat.TabIndex = 8;
+            btnThoat.Text = "Thoát ";
+            btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.AllowUserToDeleteRows = false;
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Columns.AddRange(new DataGridViewColumn[] { ID, TenLoai });
+            dataGridView.Dock = DockStyle.Fill;
+            dataGridView.Location = new Point(0, 0);
+            dataGridView.MultiSelect = false;
+            dataGridView.Name = "dataGridView";
+            dataGridView.Size = new Size(759, 290);
+            dataGridView.TabIndex = 9;
+            // 
+            // ID
+            // 
+            ID.HeaderText = "ID";
+            ID.Name = "ID";
+            // 
+            // TenLoai
+            // 
+            TenLoai.HeaderText = "Tên loại sản phẩm";
+            TenLoai.Name = "TenLoai";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(dataGridView);
+            panel2.Location = new Point(29, 148);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(759, 290);
+            panel2.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(12, 125);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(785, 324);
+            groupBox1.TabIndex = 12;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Danh sách loại sản phẩm ";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnSua);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(btnThem);
+            groupBox2.Location = new Point(12, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(785, 107);
+            groupBox2.TabIndex = 13;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Thông tin loại sản phẩm ";
+            // 
+            // btnSua
+            // 
+            btnSua.Location = new Point(289, 66);
+            btnSua.Name = "btnSua";
+            btnSua.Size = new Size(75, 23);
+            btnSua.TabIndex = 4;
+            btnSua.Text = "Sửa ";
+            btnSua.UseVisualStyleBackColor = true;
+            btnSua.Click += btnSua_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(44, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Tên loại sản phẩm (*):";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnThoat);
+            Controls.Add(btnHuyBo);
+            Controls.Add(btnLuu);
+            Controls.Add(btnXoa);
+            Controls.Add(txtTenLoai);
+            Controls.Add(panel2);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBox2);
+            Name = "Form1";
+            Text = "Loại Sản Phẩm ";
+            Load += frmLoaiSanPham_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            panel2.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtTenLoai;
+        private Button btnThem;
+        private Button btnXoa;
+        private Button btnLuu;
+        private Button btnHuyBo;
+        private Button btnThoat;
+        private DataGridView dataGridView;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn TenLoai;
+        private Panel panel2;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Button btnSua;
+        private Label label1;
+    }
+}
